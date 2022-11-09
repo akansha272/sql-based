f52a0e0e603a61313753a37b24ebc1f837af17a0
akansha272 <79163946+akansha272@users.noreply.github.com>
2022-11-09 13:19:11 +0530

    error resolved posts add

--- data/database.js
+++ data/database.js
@@ -1,21 +1,22 @@
 const mongodb = require('mongodb');
+
 const MongoClient = mongodb.MongoClient;
 
 let database;
 
-async function connect(){
-    const client = await MongoClient.connect('mongodb://localhost:27017');
-    database = client.db('blog')
+async function connectToDatabase() {
+  const client = await MongoClient.connect('mongodb://127.0.0.1:27017');
+  database = client.db('blog');
 }
 
 function getDb() {
-    if(!database){
-        throw { message: 'Database connection not established!'};
-    }
-    return database;
+  if (!database) {
+    throw { message: 'Database not connected!' };
+  }
+  return database;
 }
 
 module.exports = {
-    connectToDatabase:connect,
-    getDb: getDb
+  connectToDatabase: connectToDatabase,
+  getDb: getDb,
 };
