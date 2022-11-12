e49c3859c09ecabc6ac831569053957da320a5d9
akansha272 <79163946+akansha272@users.noreply.github.com>
2022-11-13 02:18:07 +0530

    completerd

--- routes/blog.js
+++ routes/blog.js
@@ -1,3 +1,4 @@
+const { Router } = require("express");
 const express = require("express");
 const mongodb = require("mongodb");
 
@@ -49,4 +50,58 @@ router.post("/posts", async function (req, res) {
   res.redirect("/posts");
 });
 
+router.get('/posts/:id', async function(req,res, next) {
+  let postId = req.params.id;
+
+  try{
+    postId = new ObjectId(postId);
+  } catch(error) {
+    return res.status(404).render('404')
+  }
+  
+  const post = await db.getDb().collection('posts').findOne({_id: postId}, {summary: 0})
+
+  if(!post){
+    return res.status(404).render('404');
+  }
+
+  post.humanReadableDate = post.date.toLocaleDateString('en-US', {
+    weekday: 'long',
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric'
+  });
+
+  post.date = post.date.toISOString();
+  res.render('post-detail', {post:post})
+});
+
+router.get('/posts/:id/edit', async function(req,res){
+  const postId = req.params.id;
+  const post = await db.getDb().collection('posts').findOne({_id: new ObjectId(postId)}, {title:1, summary: 1, body:1})
+
+  if(!post){
+    return res.status(404).render('404');
+  }
+
+  res.render('update-post', {post:post })
+})
+
+router.post('/posts/:id/edit', async function(req,res){
+  const postId = new ObjectId(req.params.id);
+  const result = await db.getDb().collection('posts').updateOne({_id: postId}, { $set: {
+    title:req.body.title,
+    summary:req.body.summary,
+    body:req.body.content,
+    // date: new Date()
+  }});
+
+  res.redirect('/posts');
+});
+
+router.post('/posts/:id/delete', async function(req,res){
+  const postId = new ObjectId(req.params.id);
+  const result = await db.getDb().collection('posts').deleteOne({_id:postId});
+  res.redirect('/posts');
+})
 module.exports = router;
